e9fb1e86e930e69a582f82c1e17006e7f03ac3ca
alexpap2 <plskillme0@gmail.com>
2025-08-01 11:24:23 +0300

    first commit for my test project hopefully i will learn jwt and such using typescript!

---
+++ secure-backend/src/main/java/authproject/secure_backend/SecureBackendApplication.java
@@ -0,0 +1,13 @@
+package authproject.secure_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureBackendApplication.class, args);
+	}
+
+}
---
+++ secure-backend/src/test/java/authproject/secure_backend/SecureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package authproject.secure_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
